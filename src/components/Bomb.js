32a7e96de01c2e76e1a5a6b397bcb0851e4e02df
rkim91 <richeekim@gmail.com>
2018-06-25 17:38:27 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,29 @@
-// Bomb Component Code Goes Here
+import React from "react";
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+
+  render() {
+    const message =
+      this.state.secondsLeft === 0
+        ? "Boom!"
+        : `${this.state.secondsLeft} seconds left before I go boom!`;
+    return <div>{message}</div>;
+  }
+  // render() {
+  //   if (this.state.secondsLeft === 0) {
+  //     return <div>Boom!<div>
+  //   }
+  //   else {
+  //     return <div>{this.state.secondsLeft} seconds left before I go boom!</div>;
+  //   }
+  // }
+}
+
+export default Bomb;
